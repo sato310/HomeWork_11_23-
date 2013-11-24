3479fcb80b555df09b7a0af3f60a81a256ff4bad
akira000041 <akira000041@gmail.com>
2013-11-24 11:23:25 +0900

    地図完成

--- src/com/sato310/homework_11_23/MainActivity.java
+++ src/com/sato310/homework_11_23/MainActivity.java
@@ -16,15 +16,15 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_main);
 		
 		// 天気予報（電話）
-		Button button1 = (Button)findViewById(R.id.button1);
+		Button button1 = (Button)findViewById(R.id.button);
 		button1.setOnClickListener(this);
 
 		// メール送信
-		Button button2 = (Button)findViewById(R.id.button2);
+		Button button2 = (Button)findViewById(R.id.button1);
 		button2.setOnClickListener(this);
 		
 		// 博多駅（地図）
-		Button button3 = (Button)findViewById(R.id.button3);
+		Button button3 = (Button)findViewById(R.id.button2);
 		button3.setOnClickListener(this);
 		
 	}
@@ -32,7 +32,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.button1:
+		case R.id.button:
 			// アクションを指定  ACTION_CALL（電話をかける）
 			Intent intent1 = new Intent(Intent.ACTION_CALL,Uri.parse("tel:177"));
 			// 電話番号の指定
@@ -40,7 +40,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			startActivity(intent1);
 			break;
 			
-		case R.id.button2:
+		case R.id.button1:
 			Intent intent2 = new Intent();  
 			  
 			// アクションを指定  ACTION_SENDTO:メールを送る
@@ -57,7 +57,9 @@ public class MainActivity extends Activity implements OnClickListener {
 			startActivity(intent2);
 			break;
 			
-		case R.id.button3:
+		case R.id.button2:
+			Intent intent3 = new Intent(this, MapActivity.class);
+			startActivity(intent3);
 			break;
 		}
 	}
